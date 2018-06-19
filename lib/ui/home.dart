48e3f4f646cad2dfe4543be39b6943c1dcb7c12e
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-06-19 23:00:53 +0200

    Initial commit

---
+++ lib/ui/home.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.greenAccent,
+      alignment: Alignment.center,
+//      child: new Text("Hello Containers", textDirection: TextDirection.ltr,
+//                      style: new TextStyle(
+//                          color: Colors.white,
+//                          fontSize: 34.0,
+//                          fontWeight: FontWeight.w900),),
+
+//      child: new Column(
+//        mainAxisAlignment: MainAxisAlignment.center,
+//        children: <Widget>[
+//          new Text("First item", textDirection: TextDirection.ltr,
+//            style: new TextStyle(color: Colors.white),),
+//          new Text("Second Item)", textDirection: TextDirection.ltr,
+//              style: new TextStyle(color: Colors.blueGrey),),
+//          new Container(
+//            color: Colors.deepOrange.shade50,
+//            alignment: Alignment.bottomLeft,
+//
+//              child: new Text("3rd item", textDirection: TextDirection.ltr,
+//                style: new TextStyle(color: Colors.blue, fontSize: 20.0),),
+//          )
+//        ]
+//      )
+
+
+//      child: new Row(
+//        mainAxisAlignment: MainAxisAlignment.center,
+//        children: <Widget>[
+//            new Text("Item 1", textDirection: TextDirection.ltr,
+//                style: new TextStyle(fontSize: 20.0),),
+//            new Text("Item 2", textDirection: TextDirection.ltr,
+//              style: new TextStyle(fontSize: 20.0),),
+//            new Text("Item 3", textDirection: TextDirection.ltr,
+//              style: new TextStyle(fontSize: 20.0),),
+//
+//            const Expanded(
+//              child: const Text("Item3")
+//            )
+//        ],
+//      )
+
+//        child: new Stack(
+//          alignment: Alignment.bottomCenter,
+//          children: <Widget>[
+//            const Text("Hello There"),
+//            const Text("Hey Again"),
+//            const Text("Hey nr. 1!"),
+//          ],
+//        )
+
+
+
+    );
+    // TODO: implement build
+  }
+}
